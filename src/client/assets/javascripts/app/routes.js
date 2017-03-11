006424b1142af3115d1b9a8cb3f73554eac39a76
Georgiy Ivanov <givanov@ru.ibm.com>
2017-03-11 20:51:16 +0300

    last changes

--- src/client/assets/javascripts/app/routes.js
+++ src/client/assets/javascripts/app/routes.js
@@ -9,6 +9,7 @@ export default (
   <Route path="/" component={App}>
     <IndexRoute component={GameView} />
     <Route path="404" component={NotFoundView} />
+    <Redirect from="/battleship" to="/" />
     <Redirect from="*" to="404" />
   </Route>
 );
