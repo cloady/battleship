7f7c28bd5a3defdb69ccb1d0df9b94163ff55265
Georgiy Ivanov <givanov@ru.ibm.com>
2017-03-11 20:49:55 +0300

    last changes

--- src/client/assets/javascripts/app/reducer.js
+++ src/client/assets/javascripts/app/reducer.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 
-import friends, { NAME as friendsName } from 'features/game';
+import game, { NAME as gameName } from 'features/game';
 
 export default combineReducers({
   routing,
-  [friendsName]: friends
+  [gameName]: game
 });
