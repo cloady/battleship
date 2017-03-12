54848898569188891a8f3de4dc6095b580df5daa
Georgiy Ivanov <givanov@ru.ibm.com>
2017-03-12 19:17:44 +0300

    testing fix

--- src/client/assets/javascripts/app/reducer.js
+++ src/client/assets/javascripts/app/reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 
-import game, { NAME as gameName } from 'features/game';
+import game, { NAME as gameName } from '../features/game';
 
 export default combineReducers({
   routing,
