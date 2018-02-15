78a48854082a5a35409590cd1dd2038d9c2ebff8
Georgii ivanov <xercool@MacBook-Pro.local>
2018-02-15 19:33:00 +0300

    Fixed sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { padStart } from 'lodash';
-import { coordsInArray } from '../../utils';
 import { IShipData } from '../../models';
+
+import PlayerInfo from '../PlayerInfo';
+import ShipAssets from '../ShipAssets';
 
 import './Sidebar.css';
 
@@ -22,50 +23,23 @@ export default (props: IProps) => {
      * @todo. Move to redux state.
      */
     const getScore = () => {
-      return ((hits.length - misses.length) * 10).toString();
+      return (hits.length - misses.length) * 10;
     };
 
     return (
     <div className="sidebar col-xs-12 col-sm-12 col-md-12 col-lg-4">
       <div className="container-fluid">
         <div className="playerInfo row">
-          <div className="playerInfo__display playerInfo__display-1 col-xs-6 col-md-6 col-sm-6 col-lg-6">
-                    <span className="playerInfo__score">
-                      {padStart(getScore(), 3, '0')}
-                    </span>
-            <hr className="playerInfo__divider" />
-                    <span className="playerInfo__name">
-                      Player 1
-                    </span>
-          </div>
+          <PlayerInfo score={getScore()} className="playerInfo__display-1">
+            Player1
+          </PlayerInfo>
 
-          <div className="playerInfo__display playerInfo__display-2 col-xs-6 col-md-6 col-sm-6 col-lg-6">
-                    <span className="playerInfo__score">
-                      000
-                    </span>
-            <hr className="playerInfo__divider" />
-                    <span className="playerInfo__name">
-                      Player 2
-                    </span>
-          </div>
+          <PlayerInfo score={0} className="playerInfo__display-2">
+            Player2
+          </PlayerInfo>
         </div>
 
-        { ships.map(
-          (ship, index) => <div key={index} className="assetInfo row">
-            <div className={`col-xs-6 col-md-6 col-sm-6 col-lg-6 assetInfo__ship assetInfo__ship-${ship.ship}`} />
-            <div className="col-xs-6 col-md-6 col-sm-6 col-lg-6 assetInfo__stats">
-              {
-                ship.positions.map(
-                  (pos, posIndex) => <span
-                    key={posIndex}
-                    className={`assetInfo__stat assetInfo__stat-${coordsInArray(hits, pos) ? 'hit' : 'miss'}`}
-                  />
-                )
-              }
-            </div>
-          </div>
-        ) }
-
+        <ShipAssets ships={ships} hits={hits} />
       </div>
     </div>
   );
